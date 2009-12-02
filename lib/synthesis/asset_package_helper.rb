f5ce62360183c0ae1a879721780180b7573efb42
James Yu <james@scribd.com>
2009-12-01 23:52:55 -0800

    Use import statements if sources exceed the IE stylesheet limit

--- lib/synthesis/asset_package_helper.rb
+++ lib/synthesis/asset_package_helper.rb
@@ -1,5 +1,6 @@
 module Synthesis
   module AssetPackageHelper
+    IE_STYLESHEET_LIMIT = 31 # this is the maximum number of stylesheet tags per page or import statements per style tag
     
     def should_merge?
       AssetPackage.merge_environments.include?(RAILS_ENV)
@@ -21,7 +22,25 @@ module Synthesis
         AssetPackage.targets_from_sources("javascripts", sources) : 
         AssetPackage.sources_from_targets("javascripts", sources))
         
-      sources.collect {|source| javascript_include_tag(source, options) }.join("\n")
+      if sources.size > IE_STYLESHEET_LIMIT
+        # need to use the import method so that IE doesn't just sliently drop styles
+        # after the limit has been reached
+        out = ''
+        sources.in_groups_of(IE_STYLESHEET_LIMIT).each do |group|
+          group_imports = group.map do |source|
+            if source
+              "@import url(#{stylesheet_path(source)});"
+            else
+              nil
+            end
+          end.compact.join("\n")
+          out += content_tag(:style, "<!-- #{group_imports} -->", :type => 'text/css', :media => 'all')
+        end
+        return out
+      else
+        return sources.collect { |source| stylesheet_link_tag(source, options) }.join("\n")    
+      end
+
     end
 
     def stylesheet_link_merged(*sources)
